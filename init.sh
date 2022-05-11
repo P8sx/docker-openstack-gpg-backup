2825531d4969436b178ab88cfa3b745e365d6208
P8sx <ry@phsx.pl>
2022-05-12 00:57:10 +0200

    added cron

--- init.sh
+++ init.sh
@@ -2,4 +2,11 @@
 
 # Import GPG public keys
 gpg --import /config/*.gpg
+
+if [ ! -z "$CRON" ] 
+then
+    echo "$CRON_INTERVAL /backup.sh" >> /backup.cron
+    crontab /backup.cron
+fi
+
 tail -f /dev/null
